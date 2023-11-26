81e01b2a42452987140709f057a4a24cf7506845
Ángel Bielsa <145146806+abielsaf@users.noreply.github.com>
2023-11-26 18:19:06 +0000

    Styling and code indentation

--- src/app.js
+++ src/app.js
@@ -16,26 +16,12 @@ let generateExcuse = () => {
   let who = ["The dog", "My neighbour", "His turtle", "My parrot"];
   let action = ["ate", "peed", "crushed", "broke"];
   let what = ["my homework", "the keys", "the car"];
-  let when = [
-    "before the class",
-    "right on time",
-    "when I finished",
-    "during my lunch",
-    "while I was coding"
-  ];
+  let when = ["before the class","right on time","when I finished","during my lunch","while I was coding"];
 
   let whoIndex = Math.floor(Math.random() * who.length);
   let actionIndex = Math.floor(Math.random() * action.length);
   let whatIndex = Math.floor(Math.random() * what.length);
   let whenIndex = Math.floor(Math.random() * when.length);
 
-  return (
-    who[whoIndex] +
-    " " +
-    action[actionIndex] +
-    " " +
-    what[whatIndex] +
-    " " +
-    when[whenIndex]
-  );
+  return ( who[whoIndex] + " " + action[actionIndex] + " " + what[whatIndex] + " " + when[whenIndex]);
 };
